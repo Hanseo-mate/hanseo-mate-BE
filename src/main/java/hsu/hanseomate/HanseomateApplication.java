625441c48b434c4bad4d78c32011a27019e74f73
ParkChanWoo0321 <op9563_@naver.com>
2026-07-16 20:12:20 +0900

    프로젝트 생성

---
+++ src/main/java/hsu/hanseomate/HanseomateApplication.java
@@ -0,0 +1,13 @@
+package hsu.hanseomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanseomateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanseomateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hsu/hanseomate/HanseomateApplicationTests.java
@@ -0,0 +1,13 @@
+package hsu.hanseomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanseomateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
